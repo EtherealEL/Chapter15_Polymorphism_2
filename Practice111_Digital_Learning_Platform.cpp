8ad751b23a32fdd7792bceb2bd654afa8b7975b9
EtherealEL <mjone148@aum.edu>
2026-04-24 22:54:32 -0500

    Add files via upload

---
+++ Practice111_Digital_Learning_Platform.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+class Course
+{
+public:
+    virtual void startClass() = 0;
+};
+
+class ProgrammingCourse : public Course
+{
+public:
+    void startClass()
+    {
+        cout << "Programming class has started\n";
+    }
+};
+
+class MathCourse : public Course
+{
+public:
+    void startClass()
+    {
+        cout << "Math class has started\n";
+    }
+};
+
+
+int main()
+{
+    Course* course_pointer;
+
+    ProgrammingCourse programming_object;
+    MathCourse math_object;
+
+    course_pointer = &programming_object;
+    course_pointer->startClass();
+
+    course_pointer = &math_object;
+    course_pointer->startClass();
+
+    return 0;
+}
